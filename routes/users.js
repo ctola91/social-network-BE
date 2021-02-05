82ff5af05b6c7fac0c7bb02dc355510cae4d939e
ctola91 <christian.tola@hotmail.com>
2021-02-05 19:05:21 -0400

    Add swagger to users routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,80 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *            User:
+ *               type: object
+ *               required:
+ *                   - firstName
+ *                   - lastName
+ *                   - email
+ *                   - password
+ *                   - role
+ *                   - state
+ *               properties:
+ *                   id:
+ *                       type: number
+ *                       description: The auto-generated id of the user.
+ *                   firstName:
+ *                       type: string
+ *                       description: firstName of user
+ *                   lastName:
+ *                       type: string
+ *                       description: LastName of user
+ *                   email:
+ *                       type: string
+ *                       description: Email of user
+ *                   password:
+ *                       type: string
+ *                       description: password of user
+ *                   img:
+ *                       type: string
+ *                       description: image of user
+ *                   role: 
+ *                       type: string,
+ *                       description: role of user
+ *                   state:
+ *                       type: boolean
+ *                       description: is available?
+ *               example:
+ *                   firstName: Christian
+ *                   lastName: Tola
+ *                   email: admin@gmail.com
+ *                   password: your_strong_password
+ *                   img: image.jpg
+ *                   role: ADMIN_ROLE
+ *                   state: true
+ */
 const express = require('express');
 
 const { getUsers, addUser } = require('../services/UserService');
 const app = express();
-
+/**
+ * @swagger
+ * 
+ * /users:
+ *      get:
+ *          tags:
+ *              - Users
+ *          produces:
+ *              - application/json
+ *          parameters:
+ *              - in: query
+ *                name: from
+ *                type: number
+ *              - in: query
+ *                name: limit
+ *                type: number
+ *          responses:
+ *              '200':
+ *                  description: a list of users
+ *                  content: 
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              items: 
+ *                                  $ref: '#/components/schemas/User'
+ */
 app.get('/users', async (req, res) => {
     try {
         let from = req.query.from || 0;
@@ -21,9 +93,33 @@ app.get('/users', async (req, res) => {
 });
 
 // app.get('/users/:userId', (req, res) => getUserById(req, res));
+/**
+ * @swagger
+ * 
+ * /users:
+ *      post:
+ *          tags:
+ *              - Users
+ *          produces:
+ *              - application/json
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          responses:
+ *              '201':
+ *                  description: user created
+ *                  content: 
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User' 
+ */
 app.post('/users', async (req, res) => {
     try {
         let body = req.body;
+        console.log(body);
         const user = await addUser(body);
         return res.status(201).json(user);
     } catch (e) {
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 // const morgan = require("morgan");
 require("dotenv").config();
 
@@ -7,7 +9,41 @@ const { PORT } = require("./config/config");
 
 const app = express();
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/users.js"],
+};
+
+const specs = swaggerJSDoc(options);
+
 app.use(bodyParser.json());
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true })
+);
 // middleware para subir archivos
 // app.use(fileUpload());
 // app.use(morgan("dev"));
