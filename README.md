313623212551422aec1e5e978dbfd9fa520d2219
ctola91 <christian.tola@hotmail.com>
2021-03-11 17:35:46 -0400

    add deploy settings

---
+++ README.md
@@ -0,0 +1,65 @@
+# Desplegar app en Amazon Lightsail
+
+- Crear cuenta de amazon
+
+- conectarse por ssh
+
+```bash
+cd stack
+mkdir projects
+sudo chown $USER projects
+cd projects
+git clone APP_URL
+mv web-server myapp
+sudo mkdir /opt/bitnami/projects/myapp/conf
+sudo mkdir /opt/bitnami/projects/myapp/htdocs
+sudo nano /opt/bitnami/projects/myapp/conf/httpd-prefix.conf
+#mv to stack
+```
+add this
+
+```conf
+Include "/opt/bitnami/projects/myapp/conf/httpd-app.conf"
+```
+
+```bash
+sudo nano /opt/bitnami/projects/myapp/conf/httpd-app.conf
+```
+
+add this
+
+```conf
+ProxyPass / http://127.0.0.1:3000/
+ProxyPassReverse / http://127.0.0.1:3000/
+```
+add to bitnami.conf
+```conf
+Include "/opt/bitnami/projects/myapp/conf/httpd-prefix.conf"
+```
+
+
+```bash
+sudo /opt/bitnami/ctlscript.sh restart apache
+sudo /opt/bitnami/ctlscript.sh status apache
+
+or
+
+sudo vim /opt/bitnami/apache2/conf/bitnami/conf/bitnami-apps-prefix.conf
+add to bitnami.conf
+```conf
+Include "/opt/bitnami/projects/myapp/conf/httpd-prefix.conf"
+```
+
+
+# npm install
+npm install
+# or
+sudo npm install --unsafe-perm=true
+```
+
+scripts
+"migrate:prod": "cross-env NODE_ENV=production npm run migrate",
+"start": "cross-env NODE_ENV=production node server.js"
+
+mean app credentials
+sudo cat /home/bitnami/bitnami_credentials
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,7 +3,7 @@ const app = require("../server.js");
 describe("Post Endpoints", () => {
   let token = '';
 
-  it("should create a new user", async () => {
+  it("should create a new user", async (done) => {
     const res = await request(app).post("/users").send({
         firstName: "Juan",
         lastName: "Perez",
@@ -13,9 +13,10 @@ describe("Post Endpoints", () => {
     });
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("id");
+    done();
   });
 
-  it("should login to the system", async () => {
+  it("should login to the system", async (done) => {
     const res = await request(app).post("/login").send({
       email: "juan2@sample.com",
       password: "admin123"
@@ -23,23 +24,26 @@ describe("Post Endpoints", () => {
   expect(res.statusCode).toEqual(200);
   token  = 'bearer ' + res.body.token;
   expect(res.body).toHaveProperty("token");
+  done();
   })
 
-  it('should fetch a single user', async () => {
+  it('should fetch a single user', async (done) => {
     const userId = 1;
     const res = await request(app).get(`/users/${userId}`).set('Authorization', token);
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('id');
+    done();
   });
 
-  it('should fetch all users', async () => {
+  it('should fetch all users', async (done) => {
     const res = await request(app).get('/users').set('Authorization', token);
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('users');
     expect(res.body.users).toHaveLength(1);
+    done();
   });
 
-  it('should update a user by Id', async () => {
+  it('should update a user by Id', async (done) => {
     const res = await request(app)
       .put('/users/1')
       .send({
@@ -50,9 +54,10 @@ describe("Post Endpoints", () => {
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('firstName');
+    done()
   });
 
-  it('should return status code 401 if password is not valid', async () => {
+  it('should return status code 401 if password is not valid', async (done) => {
     const res = await request(app)
       .post('/login')
       .send({
@@ -61,11 +66,13 @@ describe("Post Endpoints", () => {
       });
     expect(res.statusCode).toEqual(401);
     expect(res.body).toHaveProperty('message');
+    done();
   });
 
-  it('should delete a user', async () => {
+  it('should delete a user', async (done) => {
     const res = await request(app).delete('/users/1').set('Authorization', token);
     expect(res.statusCode).toEqual(204);
+    done();
   });
 
 //   it('should respond with status code 404 if resource is not found', async () => {
